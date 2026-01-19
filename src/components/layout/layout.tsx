bc175f9299a5704c95c6a3b56a8488af2fd0eb0e
Kunal Sharma <davidbroddy1@gmail.com>
2026-01-19 11:26:00 +0530

    `Added media query listener to open/close sidebar and rightbar on screen size change`

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 import Header from "../header/header";
 import Rightbar from "../rightbar/rightbar";
@@ -7,13 +8,34 @@ import {
   closeRightbar,
   closeSearchbox,
   closeSidebar,
+  openRightbar,
   openSearchbox,
+  openSidebar,
 } from "../../redux/slices/ui-slice";
 import SearchBox from "../ui/search-box";
 
 export default function Layout() {
   const ui = useAppSelector((state) => state.ui);
   const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(min-width: 768px)"); // Tailwind md
+
+    const handleLayout = () => {
+      if (mediaQuery.matches) {
+        dispatch(openSidebar());
+        dispatch(openRightbar());
+      } else {
+        dispatch(closeSidebar());
+        dispatch(closeRightbar());
+      }
+    };
+
+    handleLayout();
+    mediaQuery.addEventListener("change", handleLayout);
+
+    return () => mediaQuery.removeEventListener("change", handleLayout);
+  }, [dispatch]);
 
   return (
     <div className="h-screen overflow-y-hidden max-w-360 mx-auto">
@@ -35,13 +57,14 @@ export default function Layout() {
         )}
       </div>
 
+      {/* Search Box */}
       <SearchBox
         isDialogOpen={ui.searchboxOpen}
         setIsDialogOpen={openSearchbox}
         className="fixed top-1/2 left-1/2 z-70 aspect-3/1 w-11/12 max-w-xl -translate-x-1/2 -translate-y-1/2"
       />
 
-      {/* overlay while seachbox open */}
+      {/* overlay while searchbox open */}
       {ui.searchboxOpen && (
         <div
           className="fixed inset-0 z-60 bg-black/50 backdrop-blur-sm"
@@ -49,17 +72,17 @@ export default function Layout() {
         />
       )}
 
-      {/* overlay while sidebar/rightbar open */}
+      {/* overlay while sidebar/rightbar open (mobile only) */}
       <div
         className={`
-                fixed inset-0 z-40 bg-black/40 transition-opacity
-                lg:hidden
-                ${
-                  ui.sidebarOpen || ui.rightbarOpen
-                    ? "opacity-100 pointer-events-auto"
-                    : "opacity-0 pointer-events-none"
-                }
-              `}
+          fixed inset-0 z-40 bg-black/40 transition-opacity
+          lg:hidden
+          ${
+            ui.sidebarOpen || ui.rightbarOpen
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          }
+        `}
         onClick={() => {
           dispatch(closeRightbar());
           dispatch(closeSidebar());
